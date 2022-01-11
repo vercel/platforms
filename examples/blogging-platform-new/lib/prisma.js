2c2d684ba0bd55f66dabe8d6443d93b4c03326d3
Steven Tey <stevensteel97@gmail.com>
2022-01-10 19:49:48 -0800

    Revert "stashed some changes"
    This reverts commit ba377d45236d180b1685059d6b4bd1ffe0901b97.

--- examples/blogging-platform-new/lib/prisma.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
-
-export default prisma;
--- examples/blogging-platform/lib/useRequireAuth.ts
+++ examples/blogging-platform/lib/useRequireAuth.ts
@@ -1,20 +1,21 @@
-import { useSession } from "next-auth/react";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useSession } from 'next-auth/client'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function useRequireAuth() {
-  const { data: session } = useSession();
+
+  const [ session ] = useSession()
 
   const router = useRouter();
   // If auth.user is false that means we're not
   // logged in and should redirect.
   useEffect(() => {
-    if (!session && typeof session != "undefined") {
-      router.push(`/login`);
+    if(!session && typeof(session) != 'undefined') {
+      router.push(`/login`)
     }
   }, [session, router]);
 
   return session;
 }
 
-export default useRequireAuth;
+export default useRequireAuth
