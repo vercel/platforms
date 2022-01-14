bfcba13c8aabedbd907a92eb55ecdcad392f5b9a
Steven Tey <stevensteel97@gmail.com>
2022-01-13 20:16:39 -0800

    hopefully this will work :pray:

--- examples/blogging-platform/pages/_middleware.js
+++ examples/blogging-platform/pages/_middleware.js
@@ -3,20 +3,30 @@ import { NextResponse } from "next/server";
 export default function middleware(req) {
   const { pathname } = req.nextUrl;
   const hostname = req.headers.get("host");
-  console.log("hostname is ", hostname);
+
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname.replace(`.vercel.pub`, "")
       : hostname.replace(`.localhost:3000`, "");
-
-  console.log("currentHost is ", currentHost);
-  console.log("pathname is ", pathname);
 
   if (pathname.startsWith(`/_sites`)) {
     return new Response(null, { status: 404 });
   }
 
   if (!pathname.includes(".") && !pathname.startsWith("/api")) {
-    return NextResponse.rewrite(`/_sites/${currentHost}${pathname}`);
+    if (currentHost == "app") {
+      if (
+        pathname === "/login" &&
+        (req.cookies["next-auth.session-token"] ||
+          req.cookies["__Secure-next-auth.session-token"])
+      ) {
+        return NextResponse.redirect("/");
+      }
+      return NextResponse.rewrite(`/app${pathname}`);
+    } else if (currentHost == "localhost:3000" || currentHost == "") {
+      return NextResponse.rewrite(`/home${pathname}`);
+    } else {
+      return NextResponse.rewrite(`/_sites/${currentHost}${pathname}`);
+    }
   }
 }
