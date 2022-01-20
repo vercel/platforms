6a0d407fac6892e5ad570ce5f9d10d3c1ca06edc
Steven Tey <stevensteel97@gmail.com>
2022-01-19 19:40:59 -0800

    added plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import PlausibleProvider from "next-plausible";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <PlausibleProvider domain="demo.vercel.pub">
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </PlausibleProvider>
   );
 }
