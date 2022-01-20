c9b47cedb355cdc879a43f4741004a78c4bce782
Steven Tey <stevensteel97@gmail.com>
2022-01-19 18:39:58 -0800

    pushed some changes

--- examples/blogging-platform/pages/_middleware.js
+++ examples/blogging-platform/pages/_middleware.js
@@ -1,21 +1,18 @@
 import { NextResponse } from "next/server";
 
 export default function middleware(req) {
-  const { pathname } = req.nextUrl;
-  const hostname = req.headers.get("host");
+  const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
+  const hostname = req.headers.get("host"); // get hostname of request (e.g. demo.vercel.pub)
 
   if (hostname === "vercel.pub") {
+    // only for demo purposes – remove this if you want to use your root domain as the landing page
     return NextResponse.redirect("https://demo.vercel.pub");
   }
 
-  let currentHost =
+  const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname.replace(`.vercel.pub`, "")
       : hostname.replace(`.localhost:3000`, "");
-
-  if (hostname === "localhost:3000") {
-    currentHost = "demo";
-  }
 
   if (pathname.startsWith(`/_sites`)) {
     return new Response(null, { status: 404 });
@@ -31,6 +28,8 @@ export default function middleware(req) {
         return NextResponse.redirect("/");
       }
       return NextResponse.rewrite(`/app${pathname}`);
+    } else if (hostname === "localhost:3000") {
+      return NextResponse.rewrite(`/home`);
     } else {
       return NextResponse.rewrite(`/_sites/${currentHost}${pathname}`);
     }
---
+++ examples/blogging-platform/pages/home/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen bg-black">
+      <Head>
+        <title>Platforms on Vercel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="m-auto w-48">
+        <Image
+          width={512}
+          height={512}
+          src="/logo.png"
+          alt="Platforms on Vercel"
+        />
+      </div>
+    </div>
+  );
+}
