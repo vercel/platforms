829214cb9a62cbd3ecf75b572543e492d237337a
Steven Tey <stevensteel97@gmail.com>
2022-01-26 11:21:23 -0800

    removed twitter auth entirely
    just realized that it might make more sense to keep things simple and only have GH auth. Will keep the logic for having different links on [slug].js pages but for now let's get rid of the twitter next-auth provider and sign in button

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,10 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import TwitterProvider from "next-auth/providers/twitter";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
 export const authOptions = {
   providers: [
-    // Github OAuth is recommended
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
@@ -17,20 +15,6 @@ export const authOptions = {
           gh_username: profile.login,
           email: profile.email,
           image: profile.avatar_url,
-        };
-      },
-    }),
-    // Twitter OAuth is optional
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id,
-          name: profile.name,
-          username: profile.login,
-          email: profile.email,
-          image: profile.avatar_url.replace(/_normal\.(jpg|png|gif)$/, ".$1"),
         };
       },
     }),
--- pages/app/login.js
+++ pages/app/login.js
@@ -88,29 +88,6 @@ export default function Login() {
               </svg>
             )}
           </button>
-          <button
-            disabled={loading}
-            onClick={() => {
-              setLoading(true);
-              signIn("twitter");
-            }}
-            className={`${
-              loading ? "cursor-not-allowed bg-[#1da0f285]" : "bg-[#1da1f2]"
-            } group flex justify-center items-center space-x-5 w-full sm:px-4 h-16 my-2 rounded-md focus:outline-none`}
-          >
-            {loading ? (
-              <LoadingDots color="#666" />
-            ) : (
-              <svg
-                className="w-8 h-8 group-hover:animate-wiggle"
-                aria-hidden="true"
-                fill="white"
-                viewBox="0 0 24 24"
-              >
-                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-              </svg>
-            )}
-          </button>
         </div>
       </div>
     </div>
