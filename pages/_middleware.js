a54a30938173ebcb5f945b6c8d4df7b5590ca333
Steven Tey <stevensteel97@gmail.com>
2022-02-04 14:23:11 -0800

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -13,7 +13,7 @@ export default function middleware(req) {
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
           .replace(`.vercel.pub`, "")
-          .replace(`.platformize.vercel.app`, "")
+          .replace(`.platformize.vercel.app`, "") // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug – in this case, our team slug is "platformize", thus *.platformize.vercel.app works
       : hostname.replace(`.localhost:3000`, "");
 
   if (pathname.startsWith(`/_sites`)) {
