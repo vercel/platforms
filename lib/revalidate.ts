f615697ad6358b9f13e06b52bdfa6256d7310a48
Steven Tey <stevensteel97@gmail.com>
2022-05-10 15:54:36 -0700

    edited comment

--- lib/revalidate.ts
+++ lib/revalidate.ts
@@ -3,7 +3,7 @@ import { HttpMethod } from "@/types";
 export async function revalidate(domain?: string | null, slug?: string) {
   const urlPaths = [`/${slug}`, `/`];
 
-  // refer to https://solutions-on-demand-isr.vercel.app/ for more info
+  // refer to https://solutions-on-demand-isr.vercel.app/ for more info on bulk/batch revalidate
   try {
     await Promise.all(
       urlPaths.map((urlPath) =>
