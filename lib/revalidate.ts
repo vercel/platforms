0ba16dfd901ade87c1c9d4cffc6467f62dfd7cb4
Steven Tey <stevensteel97@gmail.com>
2022-05-10 15:54:18 -0700

    added comment

--- lib/revalidate.ts
+++ lib/revalidate.ts
@@ -3,6 +3,7 @@ import { HttpMethod } from "@/types";
 export async function revalidate(domain?: string | null, slug?: string) {
   const urlPaths = [`/${slug}`, `/`];
 
+  // refer to https://solutions-on-demand-isr.vercel.app/ for more info
   try {
     await Promise.all(
       urlPaths.map((urlPath) =>
