863ede34e21df846bc2c34af137eb63dfe6b1818
Steven Tey <stevensteel97@gmail.com>
2022-05-10 14:11:19 -0700

    Update _middleware.ts

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -21,13 +21,15 @@ export default function middleware(req: NextRequest) {
   if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
     return NextResponse.redirect("https://demo.vercel.pub");
   }
-
+  
+  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
+      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
+  
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? // You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
-        // You can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-        // in this case, our team slug is "platformize", thus *.platformize.vercel.app works
-        hostname
+      ? hostname
           .replace(`.vercel.pub`, "")
           .replace(`.platformize.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
