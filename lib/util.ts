ea41b3b60e8316e6dc848122e7f5b5d4a777a19f
Steven Tey <stevensteel97@gmail.com>
2022-08-12 08:51:57 -0500

    removed blurhash + plaiceholder, simplify it to a `getBlurDataURL` function

--- lib/util.ts
+++ lib/util.ts
@@ -9,3 +9,17 @@ export const truncate = (str: string, num: number) => {
   }
   return str.slice(0, num) + "...";
 };
+
+export const getBlurDataURL = async (url: string | null) => {
+  if (!url) {
+    return null;
+  }
+  const prefix = "https://res.cloudinary.com/vercel-platforms/image/upload/";
+  const suffix = url.split(prefix)[1];
+  const response = await fetch(
+    `${prefix}w_210,e_blur:5000,q_auto,f_auto/${suffix}`
+  );
+  const buffer = await response.arrayBuffer();
+  const base64 = Buffer.from(buffer).toString("base64");
+  return `data:image/png;base64,${base64}`;
+};
