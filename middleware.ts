53cb3242c0eb047ee535d539ded1ac0f948e5433
Steven Tey <stevensteel97@gmail.com>
2022-09-14 18:02:53 -0700

    improved middleware to use negative matchers

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  /*
-   * The way you configure your matcher items depend on your route structure.
-   * E.g. if you decide to put all your posts under `/posts/[postSlug]`,
-   * you'll need to add an extra matcher item "/posts/:path*".
-   * The reason we do this is to prevent the middleware from matching absolute paths
-   * like "demo.vercel.pub/_sites/steven" and have the content from `steven` be served.
-   *
-   * Here's a breakdown of each matcher item:
-   * 1. "/"               - Matches the root path of the site.
-   * 2. "/([^/.]*)"       - Matches all first-level paths (e.g. demo.vercel.pub/platforms-starter-kit)
-   *                        but exclude `/public` files by excluding paths containing `.` (e.g. /logo.png)
-   * 3. "/site/:path*"    – for app.vercel.pub/site/[siteId]
-   * 4. "/post/:path*"    – for app.vercel.pub/post/[postId]
-   * 5. "/_sites/:path*"  – for all custom hostnames under the `/_sites/[site]*` dynamic route (demo.vercel.pub, platformize.co)
-   *                        we do this to make sure "demo.vercel.pub/_sites/steven" is not matched and throws a 404.
-   */
   matcher: [
-    // Comment to force multi-line for better diffs
-    "/",
-    "/([^/.]*)",
-    "/site/:path*",
-    "/post/:path*",
-    "/_sites/:path*",
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /fonts (inside /public)
+     * 4. /examples (inside /public)
+     * 5. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api|_next|fonts|examples|[\\w-]+\\.\\w+).*)",
   ],
 };
 
