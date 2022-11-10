0a1093e50e0e81118845b602bd28d8b6789d2994
Steven Tey <stevensteel97@gmail.com>
2022-11-09 18:02:23 -0600

    Made client navbar with server component children work

--- app/(sites)/_sites/[site]/layout.tsx
+++ app/(sites)/_sites/[site]/layout.tsx
@@ -1,18 +1,11 @@
-import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-// import { useState, useEffect, useCallback } from "react";
 import { getSiteData } from "@/lib/fetchers";
 
-import type { Meta, WithChildren } from "@/types";
+import type { Meta } from "@/types";
 import Banner from "./banner";
+import Nav from "./nav";
 import { ReactNode } from "react";
-
-interface LayoutProps extends WithChildren {
-  meta?: Meta;
-  siteId?: string;
-  subdomain?: string;
-}
 
 export default async function Layout({
   params,
@@ -35,24 +28,9 @@ export default async function Layout({
       : `https://${data.subdomain}.vercel.pub`,
   } as Meta;
 
-  //   const [scrolled, setScrolled] = useState(false);
-
-  //   const onScroll = useCallback(() => {
-  //     setScrolled(window.pageYOffset > 20);
-  //   }, []);
-
-  //   useEffect(() => {
-  //     window.addEventListener("scroll", onScroll);
-  //     return () => window.removeEventListener("scroll", onScroll);
-  //   }, [onScroll]);
-
   return (
     <div>
-      <div
-        className={`fixed w-full ${
-          false ? "drop-shadow-md" : ""
-        }  top-0 left-0 right-0 h-16 bg-white z-30 transition-all ease duration-150 flex`}
-      >
+      <Nav>
         <div className="flex justify-center items-center space-x-5 h-full max-w-screen-xl mx-auto px-10 sm:px-20">
           <Link href="/" className="flex justify-center items-center">
             {meta?.logo && (
@@ -70,7 +48,7 @@ export default async function Layout({
             </span>
           </Link>
         </div>
-      </div>
+      </Nav>
       <div className="mt-20">{children}</div>
       <Banner subdomain="demo" />
     </div>
