d931382b2aa8a64cc7f1375d6073dc1b87f31762
Steven Tey <stevensteel97@gmail.com>
2023-01-18 16:30:05 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,8 @@ export const config = {
      * Match all paths except for:
      * 1. /api routes
      * 2. /_next (Next.js internals)
-     * 3. /_static (inside /public)
-     * 4. /examples (inside /public)
-     * 5. all root files inside /public (e.g. /favicon.ico)
+     * 3. /examples (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
      */
     "/((?!api/|_next/|_static/|examples/|[\\w-]+\\.\\w+).*)",
   ],
