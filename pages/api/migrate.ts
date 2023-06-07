977b556b2d1b01fd079753cd59f9fda481dc9134
Steven Tey <stevensteel97@gmail.com>
2023-06-06 19:18:02 -0500

    added comments

--- pages/api/migrate.ts
+++ pages/api/migrate.ts
@@ -2,15 +2,31 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
 import fs from "fs";
 
+/* 
+    This was the migration script we used to migrate from
+    our old database to the new Vercel Postgres database.
+    It's not needed anymore, but I'm keeping it here for
+    posterity.
+*/
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  //   // write to a file
-  //   const sites = await prisma.site.findMany();
-  //   fs.writeFileSync("sites.json", JSON.stringify(sites));
+  //   Download data from old database
+  //  const users = await prisma.user.findMany();
+  //  const accounts = await prisma.account.findMany();
+  //  const sites = await prisma.site.findMany();
+  //  const posts = await prisma.post.findMany();
+  //  const examples = await prisma.example.findMany();
 
-  // read from file
+  //  fs.writeFileSync("users.json", JSON.stringify(users));
+  //  fs.writeFileSync("accounts.json", JSON.stringify(accounts));
+  //   fs.writeFileSync("sites.json", JSON.stringify(sites));
+  //   fs.writeFileSync("posts.json", JSON.stringify(posts));
+  //   fs.writeFileSync("examples.json", JSON.stringify(examples));
+
+  // Upload data to new database
   //   const users = JSON.parse(fs.readFileSync("users.json", "utf8"));
   //   const accounts = JSON.parse(fs.readFileSync("accounts.json", "utf8"));
   //   const sites = JSON.parse(fs.readFileSync("sites.json", "utf8"));
