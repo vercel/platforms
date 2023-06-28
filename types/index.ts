b12c9079535cbf9694ed31112b0ba433ce52801a
Steven Tey <stevensteel97@gmail.com>
2023-06-28 17:16:48 -0500

    fix

--- types/index.ts
+++ types/index.ts
@@ -6,7 +6,6 @@ export * from "./icon";
 export * from "./mdx";
 export * from "./seo";
 export * from "./settings";
-export * from "./twitter";
 
 export type DomainVerificationStatusProps =
   | "Valid Configuration"
