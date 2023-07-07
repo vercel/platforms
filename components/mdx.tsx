8cf1ad97ebc4a28cf6fe958fc1396b82b3acfd70
Luis Alvarez D <luis@vercel.com>
2023-07-07 10:16:55 -0500

    Fix tweet styles (#241)

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -5,6 +5,7 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
 import { replaceLinks } from "@/lib/remark-plugins";
 import { Tweet } from "react-tweet";
 import BlurImage from "@/components/blur-image";
+import styles from './mdx.module.css'
 
 export default function MDX({ source }: { source: MDXRemoteProps }) {
   const components = {
@@ -16,7 +17,7 @@ export default function MDX({ source }: { source: MDXRemoteProps }) {
 
   return (
     <article
-      className="prose-md prose prose-stone m-auto w-11/12 dark:prose-invert sm:prose-lg sm:w-3/4"
+      className={`prose-md prose prose-stone m-auto w-11/12 dark:prose-invert sm:prose-lg sm:w-3/4 ${styles.root}`}
       suppressHydrationWarning={true}
     >
       {/* @ts-ignore */}
