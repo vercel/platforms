4132b9772f19ea5e5e87347e7e116e8db26eac37
William Felker <gndclouds@icloud.com>
2023-09-16 19:42:58 -0700

    test

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
         className="w-48"
       />
       <h1 className="text-white">
-        Edit this page on{" "}
+        Hi
         <InlineSnippet className="ml-2 bg-blue-900 text-blue-100">
           app/home/page.tsx
         </InlineSnippet>
