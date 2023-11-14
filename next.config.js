067c242b8edc59bccb8845a96b1c780c4bf4a36a
Steven Tey <stevensteel97@gmail.com>
2023-11-14 09:22:47 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["app.localhost:3000"],
+    },
+  },
   images: {
     remotePatterns: [
       { hostname: "public.blob.vercel-storage.com" },
