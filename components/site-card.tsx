a634401785df684f7f51904663ee426a8da14236
Jp <jpvalery@users.noreply.github.com>
2023-11-13 22:26:12 -0500

    fix: (minor) double class (#284)

--- components/site-card.tsx
+++ components/site-card.tsx
@@ -22,7 +22,7 @@ export default function SiteCard({ data }: { data: Site }) {
           blurDataURL={data.imageBlurhash ?? placeholderBlurhash}
         />
         <div className="border-t border-stone-200 p-4 dark:border-stone-700">
-          <h3 className="my-0 truncate font-cal text-xl font-bold tracking-wide dark:text-white dark:text-white">
+          <h3 className="my-0 truncate font-cal text-xl font-bold tracking-wide dark:text-white">
             {data.name}
           </h3>
           <p className="mt-2 line-clamp-1 text-sm font-normal leading-snug text-stone-500 dark:text-stone-400">
