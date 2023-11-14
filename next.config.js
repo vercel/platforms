3b5866acecdd75b0c9b2e5b3af15bb890a461cfa
Steven Tey <stevensteel97@gmail.com>
2023-11-14 09:18:04 -0800

    remotePatterns needs to be in images

--- next.config.js
+++ next.config.js
@@ -2,15 +2,17 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
-  remotePatterns: [
-    { hostname: "public.blob.vercel-storage.com" },
-    { hostname: "res.cloudinary.com" },
-    { hostname: "abs.twimg.com" },
-    { hostname: "pbs.twimg.com" },
-    { hostname: "avatar.vercel.sh" },
-    { hostname: "avatars.githubusercontent.com" },
-    { hostname: "www.google.com" },
-    { hostname: "flag.vercel.app" },
-    { hostname: "illustrations.popsy.co" },
-  ],
+  images: {
+    remotePatterns: [
+      { hostname: "public.blob.vercel-storage.com" },
+      { hostname: "res.cloudinary.com" },
+      { hostname: "abs.twimg.com" },
+      { hostname: "pbs.twimg.com" },
+      { hostname: "avatar.vercel.sh" },
+      { hostname: "avatars.githubusercontent.com" },
+      { hostname: "www.google.com" },
+      { hostname: "flag.vercel.app" },
+      { hostname: "illustrations.popsy.co" },
+    ]
+  },
 };
