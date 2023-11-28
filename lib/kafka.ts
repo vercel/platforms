8a403ff8ab05189f2fc8c53b19b3b16406dff9e6
kanelabs <ryan@kanelabs.com>
2023-11-28 15:30:29 +0300

    wrap all kafka production in try/catch

--- lib/kafka.ts
+++ lib/kafka.ts
@@ -27,36 +27,40 @@ export async function produceKafkaEvent(
   req: NextRequest,
   event: NextFetchEvent,
 ) {
-  const kafka = initKafka();
-  const eventProducer = kafka?.producer();
-  const url = req.nextUrl;
+  try {
+    const kafka = initKafka();
+    const eventProducer = kafka?.producer();
+    const url = req.nextUrl;
 
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers
-    .get("host")!
-    .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+    // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+    const hostname = req.headers
+      .get("host")!
+      .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
-  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
-  const path = url.pathname;
-  const searchParams = url.searchParams;
+    // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+    const path = url.pathname;
+    const searchParams = url.searchParams;
 
-  const topic = "fora_request";
+    const topic = "fora_request";
 
-  const message = {
-    hostname,
-    path,
-    searchParams,
-    country: req.geo?.country,
-    city: req.geo?.city,
-    region: req.geo?.region,
-    url: req.url,
-    ip: req.headers.get("x-real-ip"),
-    mobile: req.headers.get("sec-ch-ua-mobile"),
-    platform: req.headers.get("sec-ch-ua-platform"),
-    useragent: req.headers.get("user-agent"),
-  };
+    const message = {
+      hostname,
+      path,
+      searchParams,
+      country: req.geo?.country,
+      city: req.geo?.city,
+      region: req.geo?.region,
+      url: req.url,
+      ip: req.headers.get("x-real-ip"),
+      mobile: req.headers.get("sec-ch-ua-mobile"),
+      platform: req.headers.get("sec-ch-ua-platform"),
+      useragent: req.headers.get("user-agent"),
+    };
 
-  if (eventProducer) {
-    event.waitUntil(eventProducer.produce(topic, JSON.stringify(message)));
+    if (eventProducer) {
+      event.waitUntil(eventProducer.produce(topic, JSON.stringify(message)));
+    }
+  } catch (error) {
+    console.warn("Failed to produce event. ", error);
   }
 }
