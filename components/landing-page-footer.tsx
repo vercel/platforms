de4fad16a5a5b6cc5319093a3104789decb8c3e2
kanelabs <ryan@kanelabs.com>
2023-12-01 18:23:14 +0300

    max width footer

--- components/landing-page-footer.tsx
+++ components/landing-page-footer.tsx
@@ -1,12 +1,14 @@
 export default function LandingPageFooter() {
   return (
-    <footer className="border-t border-gray-500/10 bg-gray-900 text-gray-400">
+    <footer className=" bg-gray-900 text-gray-400">
       {/* <div className="px-16 pb-4 pt-16 ">
         <p className="font-bold">Contact</p>
         <p>team@fora.co</p>
       </div> */}
-      <div className="flex items-center justify-center p-4">
-        <p className="text-gray-650">{`©${new Date().getFullYear()} Fora Cities Inc.`}</p>
+      <div className="border-t border-gray-500/10 max-w-5xl mx-auto">
+        <div className="flex items-center justify-center p-4">
+          <p className="text-gray-650">{`©${new Date().getFullYear()} Fora Cities Inc.`}</p>
+        </div>
       </div>
     </footer>
   );
