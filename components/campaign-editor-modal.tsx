e4d2947ff57c8109336484fe7d22222cef3369c0
Lily Jordan <None>
2023-12-06 13:44:39 +0000

    some refactoring of campaign UI structure

---
+++ components/campaign-editor-modal.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { useTransition } from "react";
+import { createCampaign } from "@/lib/actions";
+import { useParams, useRouter } from "next/navigation";
+import CreateButton from "./primary-button";
+import { ethers } from "ethers";
+import Editor from "@/components/editor";
+
+
+export default function CampaignEditorModal() {
+  const router = useRouter();
+  const { subdomain } = useParams() as {
+    subdomain: string;
+  };
+  const [isPending, startTransition] = useTransition();
+
+  const data = {
+    name: '',
+    threshold: 0,
+  }
+
+  createCampaign(data, { params: { subdomain } }, null)
+    .then((campaignData) => {
+      router.push(`/city/${subdomain}/campaigns/${campaignData.id}`);
+      router.refresh();
+    })
+    .catch(console.error);
+
+  const onUpdate = () => {
+
+  };
+
+  const deploy = () => {
+
+  };
+
+  return (
+    <form
+    onSubmit={onSubmit}
+    className="mx-auto w-full rounded-md bg-white dark:bg-gray-900 md:max-w-md md:border md:border-gray-200 md:shadow dark:md:border-gray-700"
+  >
+    <div className="m.d:p-10 relative flex flex-col space-y-4 p-5">
+      <h2 className="font-cal text-2xl dark:text-white">
+        Create a new event
+      </h2>
+
+      <div className="flex flex-col space-y-2">
+        <label
+          htmlFor="name"
+          className="text-sm font-medium text-gray-700 dark:text-gray-400"
+        >
+          Name
+        </label>
+        <Input
+          name="name"
+          type="text"
+          placeholder="My Awesome Event"
+          autoFocus
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+          maxLength={64}
+          required
+        />
+      </div>
+
+      <div className="flex flex-col space-y-2">
+        <label
+          htmlFor="path"
+          className="text-sm font-medium text-gray-700 dark:text-gray-400"
+        >
+          Path
+        </label>
+        <div className="flex w-full max-w-md">
+          <div className="flex items-center rounded-r-lg border border-l-0 border-gray-200 bg-gray-100 px-3 text-sm dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400">
+            {organization.subdomain}.{process.env.NEXT_PUBLIC_ROOT_DOMAIN}/
+          </div>
+          <Input
+            name="path"
+            type="text"
+            placeholder="path"
+            value={data.path}
+            onChange={(e) => setData({ ...data, path: e.target.value })}
+            autoCapitalize="off"
+            pattern="[a-zA-Z0-9\-]+" // only allow lowercase letters, numbers, and dashes
+            maxLength={6432}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col space-y-2">
+        <label
+          htmlFor="description"
+          className="text-sm font-medium text-gray-700 dark:text-gray-400"
+        >
+          Description
+        </label>
+        <textarea
+          name="description"
+          value={data.description}
+          onChange={(e) => setData({ ...data, description: e.target.value })}
+          maxLength={280}
+          rows={3}
+          className="w-full rounded-md border border-gray-200 bg-gray-50 px-4 py-2 text-sm text-gray-900 placeholder:text-gray-400 focus:border-gray-900  focus:outline-none focus:ring-gray-900 dark:border-gray-600 dark:bg-gray-900 dark:text-white dark:placeholder-gray-700 dark:focus:ring-white"
+        />
+      </div>
+      <div className="flex flex-col space-y-2">
+        <label className="text-sm font-medium text-gray-700 dark:text-gray-400">
+          Starts At
+        </label>
+        <div className="flex">
+          <DatePicker
+            date={data.startingAtDate}
+            onSelect={(date) => {
+              setData((prev) => ({ ...prev, startingAtDate: date }));
+            }}
+          />
+          <TimePicker
+            value={data.startingAtTime}
+            onValueChange={(value) => {
+              setData((prev) => ({ ...prev, startingAtTime: value }));
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <label className="text-sm font-medium text-gray-700 dark:text-gray-400">
+          Ends At
+        </label>
+        <div className="flex">
+          <DatePicker
+            date={data.endingAtDate}
+            onSelect={(date) => {
+              setData((prev) => ({ ...prev, endingAtDate: date }));
+            }}
+          />
+          <TimePicker
+            value={data.endingAtTime}
+            onValueChange={(value) => {
+              setData((prev) => ({ ...prev, endingAtTime: value }));
+            }}
+          />
+        </div>
+      </div>
+    </div>
+    <div className="flex items-center justify-end rounded-b-lg border-t border-gray-200 bg-gray-50 p-3 dark:border-gray-700 dark:bg-gray-800 md:px-10">
+      <FormButton text={"Create Event"} />
+    </div>
+  </form>
+  );
+}
