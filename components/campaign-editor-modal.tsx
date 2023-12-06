f8e8afb451e9a990c6ec7ab2c498c97909e7c904
Lily Jordan <None>
2023-12-06 17:29:59 +0000

    campaign editor fashioned off post editor

--- components/campaign-editor-modal.tsx
+++ components/campaign-editor-modal.tsx
@@ -1,32 +1,53 @@
-"use client";
+// "use client";
 
 import { useTransition } from "react";
 import { createCampaign, updateCampaign, launchCampaign } from "@/lib/actions";
 import { useParams, useRouter } from "next/navigation";
 import { Campaign } from "@prisma/client";
-import CreateButton from "./primary-button";
 import { ethers } from "ethers";
-import Editor from "@/components/editor";
+import CampaignEditor from "@/components/campaign-editor";
+import { notFound } from "next/navigation";
 
 
-export default function CampaignEditorModal(
+export default async function CampaignEditorModal(
   props: { campaign: Campaign | undefined }
 ) {
   const router = useRouter();
   const { subdomain } = useParams() as {
     subdomain: string;
   };
-  const [isPending, startTransition] = useTransition();
-
-  const data = {
-    name: 'test name',
-    threshold: 0,
-  }
+  let newCampaign: Campaign;
 
   if (props.campaign === undefined) {
     // TODO will this need to be async?
-    createCampaign(data, { params: { subdomain } }, null)
+    const initialData = {
+      name: 'test name',
+      threshold: 0,
+    }
+    newCampaign = await createCampaign(
+      initialData, { params: { subdomain } }, null)
     .catch(console.error);
+  }
+
+  const data = await prisma.post.findUnique({
+    where: {
+      id: props.campaign ? props.campaign.id : newCampaign!.id
+    },
+    include: {
+      organization: {
+        select: {
+          subdomain: true,
+        },
+      },
+    },
+  });
+
+  if (!data) {
+    notFound();
+  }
+
+  if (!data.published) {
+    notFound();
   }
 
   const onUpdate = async () => {
@@ -78,118 +99,5 @@ export default function CampaignEditorModal(
     })
   };
 
-  return (
-    <form
-    onSubmit={onUpdate}
-    className="mx-auto w-full rounded-md bg-white dark:bg-gray-900 md:max-w-md md:border md:border-gray-200 md:shadow dark:md:border-gray-700"
-  >
-    <div className="m.d:p-10 relative flex flex-col space-y-4 p-5">
-      <h2 className="font-cal text-2xl dark:text-white">
-        Create a new event
-      </h2>
-
-      <div className="flex flex-col space-y-2">
-        <label
-          htmlFor="name"
-          className="text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
-          Name
-        </label>
-        <Input
-          name="name"
-          type="text"
-          placeholder="My Awesome Event"
-          autoFocus
-          value={data.name}
-          onChange={(e) => setData({ ...data, name: e.target.value })}
-          maxLength={64}
-          required
-        />
-      </div>
-
-      <div className="flex flex-col space-y-2">
-        <label
-          htmlFor="path"
-          className="text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
-          Path
-        </label>
-        <div className="flex w-full max-w-md">
-          <div className="flex items-center rounded-r-lg border border-l-0 border-gray-200 bg-gray-100 px-3 text-sm dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400">
-            {organization.subdomain}.{process.env.NEXT_PUBLIC_ROOT_DOMAIN}/
-          </div>
-          <Input
-            name="path"
-            type="text"
-            placeholder="path"
-            value={data.path}
-            onChange={(e) => setData({ ...data, path: e.target.value })}
-            autoCapitalize="off"
-            pattern="[a-zA-Z0-9\-]+" // only allow lowercase letters, numbers, and dashes
-            maxLength={6432}
-            required
-          />
-        </div>
-      </div>
-
-      <div className="flex flex-col space-y-2">
-        <label
-          htmlFor="description"
-          className="text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
-          Description
-        </label>
-        <textarea
-          name="description"
-          value={data.description}
-          onChange={(e) => setData({ ...data, description: e.target.value })}
-          maxLength={280}
-          rows={3}
-          className="w-full rounded-md border border-gray-200 bg-gray-50 px-4 py-2 text-sm text-gray-900 placeholder:text-gray-400 focus:border-gray-900  focus:outline-none focus:ring-gray-900 dark:border-gray-600 dark:bg-gray-900 dark:text-white dark:placeholder-gray-700 dark:focus:ring-white"
-        />
-      </div>
-      <div className="flex flex-col space-y-2">
-        <label className="text-sm font-medium text-gray-700 dark:text-gray-400">
-          Starts At
-        </label>
-        <div className="flex">
-          <DatePicker
-            date={data.startingAtDate}
-            onSelect={(date) => {
-              setData((prev) => ({ ...prev, startingAtDate: date }));
-            }}
-          />
-          <TimePicker
-            value={data.startingAtTime}
-            onValueChange={(value) => {
-              setData((prev) => ({ ...prev, startingAtTime: value }));
-            }}
-          />
-        </div>
-      </div>
-      <div className="flex flex-col space-y-2">
-        <label className="text-sm font-medium text-gray-700 dark:text-gray-400">
-          Ends At
-        </label>
-        <div className="flex">
-          <DatePicker
-            date={data.endingAtDate}
-            onSelect={(date) => {
-              setData((prev) => ({ ...prev, endingAtDate: date }));
-            }}
-          />
-          <TimePicker
-            value={data.endingAtTime}
-            onValueChange={(value) => {
-              setData((prev) => ({ ...prev, endingAtTime: value }));
-            }}
-          />
-        </div>
-      </div>
-    </div>
-    <div className="flex items-center justify-end rounded-b-lg border-t border-gray-200 bg-gray-50 p-3 dark:border-gray-700 dark:bg-gray-800 md:px-10">
-      <FormButton text={"Create Event"} />
-    </div>
-  </form>
-  );
+  return <CampaignEditor post={data} />;
 }
---
+++ components/campaign-editor/props.ts
@@ -0,0 +1,45 @@
+import { handleImageUpload } from "./utils";
+import { EditorProps } from "@tiptap/pm/view";
+
+export const TiptapEditorProps: EditorProps = {
+  attributes: {
+    class:
+      "prose prose-stone prose-headings:font-cal focus:outline-none max-w-full dark:prose-invert",
+  },
+  handleDOMEvents: {
+    keydown: (_view, event) => {
+      // prevent default event listeners from firing when slash command is active
+      if (["ArrowUp", "ArrowDown", "Enter"].includes(event.key)) {
+        const slashCommand = document.querySelector("#slash-command");
+        if (slashCommand) {
+          return true;
+        }
+      }
+    },
+  },
+  handlePaste: (view, event) => {
+    if (
+      event.clipboardData &&
+      event.clipboardData.files &&
+      event.clipboardData.files[0]
+    ) {
+      event.preventDefault();
+      const file = event.clipboardData.files[0];
+      return handleImageUpload(file, view, event);
+    }
+    return false;
+  },
+  handleDrop: (view, event, _slice, moved) => {
+    if (
+      !moved &&
+      event.dataTransfer &&
+      event.dataTransfer.files &&
+      event.dataTransfer.files[0]
+    ) {
+      event.preventDefault();
+      const file = event.dataTransfer.files[0];
+      return handleImageUpload(file, view, event);
+    }
+    return false;
+  },
+};
