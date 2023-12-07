173d994cdfafa42942bca8093d58aa83157f70c3
Lily Jordan <None>
2023-12-07 13:42:38 +0000

    super basic rendering of campaign form

--- app/app/(dashboard)/city/[subdomain]/campaigns/[id]/page.tsx
+++ app/app/(dashboard)/city/[subdomain]/campaigns/[id]/page.tsx
@@ -2,19 +2,12 @@ import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
 import notFound from "../../not-found";
-// import { useForm } from "react-hook-form";
-// import {
-//   Form,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
 import { UpdateCampaignSchema } from "@/lib/schema";
 import { updateCampaign } from "@/lib/actions";
 import { zodResolver } from "@hookform/resolvers/zod";
+import CampaignForm from "@/components/create-campaign-form";
 
 
 export default async function CampaignPage({
@@ -37,105 +30,64 @@ export default async function CampaignPage({
     },
   });
 
-  // const form = useForm<z.infer<typeof UpdateCampaignSchema>>({
-  //   resolver: zodResolver(UpdateCampaignSchema),
-  // });
-
   if (!campaign || !campaign.organization) {
     return notFound();
   }
 
-  async function onSubmit(data: z.infer<typeof UpdateCampaignSchema>) {
-    console.log("on Submit");
-
-    const result = await updateCampaign(
-      data,
-      { params: { subdomain: params.subdomain as string } },
-      null,
-    );
-    console.log(result);
-
-    toast({
-      title: "Campaign launched!"
-    });
-  }
-
   return (
     <div>
-      <p>
-        {campaign.name}
-      </p>
-      <p>
-        {`threshold: ${campaign.threshold}`}
-      </p>
-      <p>
-        {`content: ${campaign.content}`}
-      </p>
-      <p>
-        {`created ${campaign.createdAt.toLocaleString(undefined, {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: undefined,
-          timeZoneName: undefined
-        })}`}
-      </p>
-      <p>
-        {`last update ${campaign.updatedAt.toLocaleString(undefined, {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: undefined,
-          timeZoneName: undefined
-        })}`}
-      </p>
-      <p>
-        {`deployed: ${campaign.deployed}`}
-      </p>
-      <p>
-        {`timeDeployed: ${campaign.timeDeployed}`}
-      </p>
-      <p>
-        {`deployedAddress: ${campaign.deployedAddress}`}
-      </p>
-      <p>
-        {`sponsorEthAddress: ${campaign.sponsorEthAddress}`}
-      </p>
-      <p>
-        {`contributions: ${campaign.contributions}`}
-      </p>
-      <p>
-        {`organization: ${campaign.organization.name}`}
-      </p>
+      <div>
+        <p>
+          {campaign.name}
+        </p>
+        <p>
+          {`threshold: ${campaign.threshold}`}
+        </p>
+        <p>
+          {`content: ${campaign.content}`}
+        </p>
+        <p>
+          {`created ${campaign.createdAt.toLocaleString(undefined, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: undefined,
+            timeZoneName: undefined
+          })}`}
+        </p>
+        <p>
+          {`last update ${campaign.updatedAt.toLocaleString(undefined, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: undefined,
+            timeZoneName: undefined
+          })}`}
+        </p>
+        <p>
+          {`deployed: ${campaign.deployed}`}
+        </p>
+        <p>
+          {`timeDeployed: ${campaign.timeDeployed}`}
+        </p>
+        <p>
+          {`deployedAddress: ${campaign.deployedAddress}`}
+        </p>
+        <p>
+          {`sponsorEthAddress: ${campaign.sponsorEthAddress}`}
+        </p>
+        <p>
+          {`contributions: ${campaign.contributions}`}
+        </p>
+        <p>
+          {`organization: ${campaign.organization.name}`}
+        </p>
+      </div>
+      <CampaignForm subdomain={params.subdomain} />
     </div>
-    // <Form {...form}>
-    //   <form
-    //     onSubmit={form.handleSubmit(onSubmit)}
-    //     className="w-full space-y-6 rounded-md bg-gray-200/80 px-6 py-6 backdrop-blur-lg  dark:bg-gray-900/80 md:max-w-md md:border md:border-gray-200 md:shadow dark:md:border-gray-700"
-    //   >
-    //     <FormField
-    //       control={form.control}
-    //       name="name"
-    //       render={({ field }) => (
-    //         <FormItem>
-    //           <FormLabel>Property Name</FormLabel>
-    //           <Input {...field} />
-    //           <FormMessage />
-    //         </FormItem>
-    //       )}
-    //     />
-    //     <div>
-    //       <FormLabel>Address</FormLabel>
-    //       <GeocodeInput />
-    //     </div>
-    //     <PrimaryButton loading={pending} type="submit">
-    //       Submit
-    //     </PrimaryButton>
-    //   </form>
-    // </Form>
   );
 }
--- components/create-campaign-form.tsx
+++ components/create-campaign-form.tsx
@@ -1,42 +1,68 @@
 "use client";
 
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { Input } from '@/components/ui/input';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { UpdateCampaignSchema } from '@/lib/schema';
-import { Campaign, Organization } from "@prisma/client";
-import { updateCampaign } from '@/lib/actions';
-import { useParams } from "next/navigation";
+import React, { useState } from "react";
+import { updateCampaign } from "@/lib/actions";
 
 
-interface Data {
-  name: string,
-  threshold: number,
-  content: string
+interface FormState {
+  name: string;
+  threshold: number;
+  content: string;
 }
 
+export default function CampaignForm({ subdomain }: { subdomain: string }) {
+  const [formState, setFormState] = useState<FormState>({ name: "", threshold: 0, content: "" });
 
-export default function CampaignEditorForm(
-  campaign: Campaign,
-  organization: Organization
-) {
-  const [name, setName] = useState(campaign.name);
-  const [threshold, setThreshold] = useState(campaign.threshold);
-  const [content, setContent] = useState(campaign.content);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: zodResolver(UpdateCampaignSchema),
-  });
-
-  const { subdomain } = useParams() as { subdomain: string };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await updateCampaign(
+        formState,
+        { params: { subdomain } },
+        null,
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error updating campaign', error);
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input {...register('name')} value={name} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} />
-      <Input {...register('threshold')} value={threshold} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setThreshold(e.target.value)} />
-      <Input {...register('content')} value={content} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setContent(e.target.value)} />
-      <button type="submit">Update Campaign</button>
+    <form onSubmit={handleSubmit} className="flex flex-col space-y-4 mt-6 text-gray-800">
+      <input
+        type="text"
+        name="name"
+        value={formState.name}
+        onChange={handleChange}
+        placeholder="Name"
+        className="p-2 border border-gray-300 rounded-md"
+      />
+      <input
+        type="number"
+        name="threshold"
+        value={formState.threshold}
+        onChange={handleChange}
+        placeholder="Threshold"
+        className="p-2 border border-gray-300 rounded-md"
+      />
+      <input
+        type="text"
+        name="content"
+        value={formState.content}
+        onChange={handleChange}
+        placeholder="Content"
+        className="p-2 border border-gray-300 rounded-md"
+      />
+      <button 
+        type="submit"
+        className="p-2 text-white rounded-md"
+      >
+        Submit
+      </button>
     </form>
   );
-}
+};
