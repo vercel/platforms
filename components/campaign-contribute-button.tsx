3471101e3a07e563da50499f8ae05ec0ba9a4a22
Lily Jordan <None>
2023-12-08 21:02:57 +0000

    handle ethers logic in campaign page (currently doesn't work because of next 13 server/client separation)

--- components/campaign-contribute-button.tsx
+++ components/campaign-contribute-button.tsx
@@ -4,54 +4,50 @@ import CampaignContract from '@/protocol/campaigns/out/Campaign.sol/Campaign.jso
 import { ethers } from "ethers";
 import { Campaign } from "@prisma/client";
 import { Button } from "./ui/button";
+import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
+import React, { useState } from 'react';
 
 
-export default function LaunchCampaignButton(
-  props: { campaign: Campaign, subdomain: string }
-) {
-  const launch = async () => {
-      try {
-        if (!window.ethereum) {
-          throw new Error("Please install MetaMask or another wallet to create a campaign.");
-        }
+interface CampaignContributeButtonProps {
+  campaign: Campaign;
+  subdomain: string;
+  onContribute: (amount: string, campaign: Campaign) => Promise<void>;
+}
 
-        const provider = new ethers.BrowserProvider(window.ethereum);
-        await provider.send("eth_requestAccounts", []);
-        const signer = await provider.getSigner();
+export default function CampaignContributeButton({ 
+  campaign, 
+  subdomain, 
+  onContribute 
+}: CampaignContributeButtonProps) {
+  const [amount, setAmount] = useState('');
 
-        const campaignABI = CampaignContract.abi;
-        const campaignBytecode = CampaignContract.bytecode;
+  const isValidAmount = () => {
+    const num = parseFloat(amount);
+    return !isNaN(num) && num > 0;
+  };
 
-        const creatorAddress = signer.address;
-        const threshold = ethers.parseUnits(props.campaign.threshold.toString(), "ether");
-        const name = props.campaign.name;
-
-        const campaignFactory = new ethers.ContractFactory(campaignABI, campaignBytecode, signer);
-        const campaignInstance = await campaignFactory.deploy(creatorAddress, threshold, name);
-        await campaignInstance.waitForDeployment();
-        const deployedAddress = await campaignInstance.getAddress();
-
-        const data = {
-          id: props.campaign.id,
-          sponsorEthAddress: creatorAddress,
-          deployedAddress: deployedAddress,
-          // deployedAddress: creatorAddress,  // Debug,
-          deployed: true,
-        };
-
-        toast.success(`Campaign deployed at ${deployedAddress}`);
-
-        launchCampaign(data, { params: { subdomain: props.subdomain } }, null);
-      } catch (error: any) {
-        console.error(error);
-        toast.error(error);
-      }
+  const handleContribution = () => {
+    if (isValidAmount()) {
+      onContribute(amount, campaign);
+    }
   };
 
   return (
-    <Button onClick={launch}>
-      Launch campaign
-    </Button>
+    <div>
+      <Input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        placeholder="Amount"
+      />
+      <Button
+        onClick={handleContribution}
+        disabled={!isValidAmount()}
+        className={`mt-2 ${isValidAmount() ? 'hover:bg-gray-700' : 'bg-gray-500'}`}
+      >
+        Contribute
+      </Button>
+    </div>
   );
 }
--- components/launch-campaign-button.tsx
+++ components/launch-campaign-button.tsx
@@ -1,57 +1,16 @@
-"use client";
-
-import { launchCampaign, updateCampaign } from "@/lib/actions";
-import CampaignContract from '@/protocol/campaigns/out/Campaign.sol/Campaign.json';
-import { ethers } from "ethers";
+import React from 'react';
 import { Campaign } from "@prisma/client";
 import { Button } from "./ui/button";
-import { toast } from "sonner";
 
+interface LaunchCampaignButtonProps {
+  campaign: Campaign;
+  subdomain: string;
+  onLaunch: (campaign: Campaign) => Promise<void>;
+}
 
-export default function LaunchCampaignButton(
-  props: { campaign: Campaign, subdomain: string }
-) {
-  const launch = async () => {
-      try {
-        if (!window.ethereum) {
-          throw new Error("Please install MetaMask or another wallet to create a campaign.");
-        }
-
-        const provider = new ethers.BrowserProvider(window.ethereum);
-        await provider.send("eth_requestAccounts", []);
-        const signer = await provider.getSigner();
-
-        const campaignABI = CampaignContract.abi;
-        const campaignBytecode = CampaignContract.bytecode;
-
-        const creatorAddress = signer.address;
-        const threshold = ethers.parseUnits(props.campaign.threshold.toString(), "ether");
-        const name = props.campaign.name;
-
-        const campaignFactory = new ethers.ContractFactory(campaignABI, campaignBytecode, signer);
-        const campaignInstance = await campaignFactory.deploy(creatorAddress, threshold, name);
-        await campaignInstance.waitForDeployment();
-        const deployedAddress = await campaignInstance.getAddress();
-
-        const data = {
-          id: props.campaign.id,
-          sponsorEthAddress: creatorAddress,
-          deployedAddress: deployedAddress,
-          // deployedAddress: creatorAddress,  // Debug,
-          deployed: true,
-        };
-
-        toast.success(`Campaign deployed at ${deployedAddress}`);
-
-        launchCampaign(data, { params: { subdomain: props.subdomain } }, null);
-      } catch (error: any) {
-        console.error(error);
-        toast.error(error);
-      }
-  };
-
+export default function LaunchCampaignButton({ campaign, subdomain, onLaunch }: LaunchCampaignButtonProps) {
   return (
-    <Button onClick={launch}>
+    <Button onClick={() => onLaunch(campaign)}>
       Launch campaign
     </Button>
   );
