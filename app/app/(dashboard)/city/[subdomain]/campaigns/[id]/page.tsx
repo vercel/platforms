b75d6ea5b44f8625814fe2e0cf74388a9873cc66
Lily Jordan <None>
2023-12-10 11:46:36 +0000

    separate client component to read from the campaign contract

--- app/app/(dashboard)/city/[subdomain]/campaigns/[id]/page.tsx
+++ app/app/(dashboard)/city/[subdomain]/campaigns/[id]/page.tsx
@@ -2,15 +2,7 @@ import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
 import notFound from "../../not-found";
-import LaunchCampaignButton from "@/components/launch-campaign-button";
-import CampaignContributeButton from "@/components/campaign-contribute-button";
-import CampaignForm from "@/components/edit-campaign-form";
-import { ethers } from "ethers";
-import { toast } from "sonner";
-import CampaignContract from '@/protocol/campaigns/out/Campaign.sol/Campaign.json';
-import { Campaign } from "@prisma/client";
-import { launchCampaign } from "@/lib/actions";
-import useEthereum from "@/hooks/useEthereum";
+import CampaignPageContent from "@/components/campaign-page-content";
 
 
 export default async function CampaignPage({
@@ -22,8 +14,6 @@ export default async function CampaignPage({
   if (!session) {
     redirect("/login");
   }
-
-  const { getContributionTotal } = useEthereum();
 
   const campaign = await prisma.campaign.findUnique({
     where: {
@@ -39,72 +29,8 @@ export default async function CampaignPage({
     return notFound();
   }
 
-  let totalContributions;
-  if (campaign.deployed) {
-    totalContributions = await getContributionTotal(campaign.deployedAddress);
-  }
-
-  return (
-    <div>
-      <div>
-        <h1 className="text-2xl font-bold">
-          {campaign.name}
-        </h1>
-        <p>
-          {`threshold: ${campaign.threshold} ETH`}
-        </p>
-        <p>
-          {`content: ${campaign.content}`}
-        </p>
-        <p>
-          {`created ${campaign.createdAt.toLocaleString(undefined, {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: undefined,
-            timeZoneName: undefined
-          })}`}
-        </p>
-        <p>
-          {`last updated ${campaign.updatedAt.toLocaleString(undefined, {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: undefined,
-            timeZoneName: undefined
-          })}`}
-        </p>
-        <p>
-          {campaign.deployed
-          ? `Deployed ${campaign.timeDeployed.toLocaleString(undefined, {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: undefined,
-            timeZoneName: undefined
-          })} at ${campaign.deployedAddress} by ${campaign.sponsorEthAddress}`
-          : "Not deployed"}
-        </p>
-        <p>
-          {`Raised so far: ${campaign.contributions} ETH`}
-        </p>
-      </div>
-      <CampaignForm id={params.id} subdomain={params.subdomain} />
-      {!campaign.deployed &&
-            <LaunchCampaignButton campaign={campaign} subdomain={params.subdomain} />
-      }
-      {campaign.deployed && (
-        <CampaignContributeButton
-          campaign={campaign}
-          subdomain={params.subdomain}
-        />
-      )}
-    </div>
-  );
+  return <CampaignPageContent
+    campaign={campaign}
+    subdomain={params.subdomain}
+  />
 }
---
+++ components/campaign-page-content.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import LaunchCampaignButton from "@/components/launch-campaign-button";
+import CampaignContributeButton from "@/components/campaign-contribute-button";
+import CampaignForm from "@/components/edit-campaign-form";
+import useEthereum from "@/hooks/useEthereum";
+import { Campaign } from "@prisma/client";
+import { useState, useEffect } from 'react';
+
+
+export default function CampaignPageContent(
+  {campaign, subdomain}: {campaign: Campaign, subdomain: string}
+) {
+  const { getContributionTotal } = useEthereum();
+  const [totalContributions, setTotalContributions] = useState(null);
+
+  useEffect(() => {
+    async function fetchTotalContributions() {
+      if (campaign.deployed) {
+        const total = await getContributionTotal(campaign.deployedAddress!);
+        setTotalContributions(total);
+      }
+    }
+    fetchTotalContributions();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [campaign]);
+
+  return (
+    <div>
+      <div>
+        <h1 className="text-2xl font-bold">
+          {campaign.name}
+        </h1>
+        <p>
+          {`threshold: ${campaign.threshold} ETH`}
+        </p>
+        <p>
+          {`content: ${campaign.content}`}
+        </p>
+        <p>
+          {`created ${campaign.createdAt.toLocaleString(undefined, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: undefined,
+            timeZoneName: undefined
+          })}`}
+        </p>
+        <p>
+          {`last updated ${campaign.updatedAt.toLocaleString(undefined, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: undefined,
+            timeZoneName: undefined
+          })}`}
+        </p>
+        <p>
+          {campaign.deployed
+          ? `Deployed ${campaign.timeDeployed!.toLocaleString(undefined, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: undefined,
+            timeZoneName: undefined
+          })} at ${campaign.deployedAddress} by ${campaign.sponsorEthAddress}`
+          : "Not deployed"}
+        </p>
+        {totalContributions &&
+          <p>
+            {`Raised so far: ${totalContributions} ETH`}
+          </p>
+        }
+      </div>
+      <CampaignForm id={campaign.id} subdomain={subdomain} />
+      {!campaign.deployed &&
+            <LaunchCampaignButton campaign={campaign} subdomain={subdomain} />
+      }
+      {campaign.deployed && (
+        <CampaignContributeButton
+          campaign={campaign}
+          subdomain={subdomain}
+        />
+      )}
+    </div>
+  );
+}
