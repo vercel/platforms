4bdbe5d68db33702701bc5683a49a65b5764cff1
Joel Gabriel (Desktop) <joel@inspacexr.com>
2024-03-30 11:26:01 +1100

    style: added comma back which was auto-removed

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,7 @@ export const config = {
      * 3. /_static (inside /public)
      * 4. all root files inside /public (e.g. /favicon.ico)
      */
-    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
   ],
 };
 
