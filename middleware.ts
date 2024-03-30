04e8b026d64c6bf578b2afeeaadd5e72a968189c
Joel Gabriel (Desktop) <joel@inspacexr.com>
2024-03-30 11:24:39 +1100

    chore: added comment block back to matcher

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export const config = {
-  matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
+  matcher: [
+     /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"
+  ],
 };
 
 export default async function middleware(req: NextRequest) {
